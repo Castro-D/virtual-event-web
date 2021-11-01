2fa6b660b6b7e6d6258e60d7b844923548b1e004
Castro-D <castro.dh91@gmail.com>
2021-11-01 16:22:00 -0300

    add jest and babel dependencies

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  verbose: true,
+  rootDir: 'src',
+  coverageDirectory: '../coverage/',
+  testPathIgnorePatterns: ['/node_modules/'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+};
