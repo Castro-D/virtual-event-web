dff7b7664887b931551b2fa92fa45dab2b5c3007
Castro-D <castro.dh91@gmail.com>
2021-11-01 21:09:26 -0300

    split index file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import mapAttendees from './mapper/attendeeMapper.js';
-import saveAttendee from './storage/attendee.js';
-import { getFormData, openModal, getEmptyValues } from './ui/form.js';
-import validateValues from './utilities/validator.js';
-
-function handleOnClick(attendee) {
-  const entity = mapAttendees(attendee);
-  const emptyStrings = getEmptyValues();
-  validateValues(emptyStrings, saveAttendee, openModal, entity);
-}
-
-function initialize() {
-  getFormData(handleOnClick);
-}
+import initialize from './event.js'
 
 initialize();
